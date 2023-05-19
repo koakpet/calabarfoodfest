e07b74e0abb9f468804f0727296c75c4be08f506
koakpet <koakpet@yahoo.co.uk>
2023-05-19 11:24:06 +0100

    X

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const _ = require('lodash');
+
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
-app.get("/", function(req,res){
-  res.sendFile(__dirname + "/index.html");
-})
 
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/about', require('./routes/about'));
+app.use('/register', require('./routes/register'));
+app.use('/contact', require('./routes/contact'));
+
+
+
+// Server
 app.listen(process.env.PORT || 3000, function(req, res){
-  console.log("Server is running on port 3000");
-})
+  console.log("Server is running on Port 3000");
+});
---
+++ routes/contact.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Routes
+router.get('', function(req,res){
+  res.render('contact');
+});
+
+
+  module.exports = router;
+  
