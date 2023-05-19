9fd64deaa3d0729a8bc2f40033354e441d8c569e
koakpet <koakpet@yahoo.co.uk>
2023-05-19 23:57:46 +0100

    Completed work on about page for presentation

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.use('/about', require('./routes/about'));
 app.use('/register', require('./routes/register'));
 app.use('/contact', require('./routes/contact'));
 
-
+app.get('/test', function(req, res){
+  res.render('test');
+});
 
 // Server
 app.listen(process.env.PORT || 3000, function(req, res){
