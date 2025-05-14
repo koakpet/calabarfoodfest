b9ae365379c194cd37da87202c9697c048ead706
koakpet <koakpet@yahoo.co.uk>
2025-05-14 15:34:11 +0100

    trying to make it vercel deployable

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const _ = require('lodash');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 // Routes
-app.use('/', require('./routes/home'));
-app.use('/about', require('./routes/about'));
-app.use('/register', require('./routes/register'));
-app.use('/contact', require('./routes/contact'));
+app.use("/", require("./routes/home"));
+app.use("/about", require("./routes/about"));
+app.use("/register", require("./routes/register"));
+app.use("/contact", require("./routes/contact"));
 
 // Server
-app.listen(process.env.PORT || 3000, function(req, res){
+app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Server is running on Port 3000");
 });
